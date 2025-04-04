6b4cedc70ba8031056120d338121ff54e6f6261a
Tiago Duarte da Cunha <tiago.cunha@aluno.fmpsc.edu.br>
2025-04-04 09:49:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const index = document.getElementById("index");
+const sobre = document.getElementById("sobre");
+const servicos = document.getElementById("servicos");
+const contato = document.getElementById("contato");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("mousedown", () =>{
+
+});
